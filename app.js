f871a212b5a7536b2148af160613fcb0b8665bcf
Jozef Woroniecki <jozefw@sbcglobal.net>
2022-05-29 20:21:03 +0000

    add express and thunderclient

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,resp)=>{
-    resp.status(200).send("Server response from root page")
+    resp
+    .status(200)
+    .json({message:"Server response from root page",app:"AuNatureaux"})
+})
+
+app.post('/',(req,resp)=>{
+    res.send("You can Post to here")
 })
 
 const port = 3000;
 app.listen(port,()=>{
-    console.log("App running on PORT ${port}...")
+    console.log(`App running on PORT ${port}...`)
 })
