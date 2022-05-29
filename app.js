33131b7606fb78a791bc81e609a000f191cca646
Jozef Woroniecki <jozefw@sbcglobal.net>
2022-05-29 21:41:31 +0000

    something is broke with syntax

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
 const fs = require('fs');
+const express = require('express');
 
 const app = express();
 
-const tours = JSON.parse(fs.readFileSync(`$(__dirname)/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get('/api/v1/tours',(req,resp)=>{
-    resp
-    .status(200)
-    .json({
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
         status:'success',
-        tours:tours
+        results:tours.length,
+        data:{tours}
 })
 
-app.post('/',(req,resp)=>{
-    res.send("You can Post to here")
+app.post('/api/v1/tours ',(req,res)=>{
+    console.log(req.body)
+    res.send("Done")
 })
 
 const port = 3000;
