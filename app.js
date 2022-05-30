4d20fbc1704b5af1e5c50c1a7c530b99fda2567b
Jozef <Jozefw@sbcglobal.net>
2022-05-30 08:07:56 -0700

    add new post

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
@@ -11,10 +12,21 @@ app.get('/api/v1/tours',(req,res)=>{
         results:tours.length,
         data:{tours}
 })
+})
 
-app.post('/api/v1/tours ',(req,res)=>{
-    console.log(req.body)
-    res.send("Done")
+app.post('/api/v1/tours',(req,res)=>{
+    console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({newId},req.body);
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
+        res
+        .status(201)
+        .json({
+            status: 'success',
+            data: {tour:newTour},
+        })
+    });
 })
 
 const port = 3000;
