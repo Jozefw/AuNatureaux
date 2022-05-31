3800686ca0d905f5cc44b480509d6f3343827c2f
Jozef <Jozefw@sbcglobal.net>
2022-05-31 13:02:32 -0700

    add refactoring

--- app.js
+++ app.js
@@ -6,30 +6,92 @@ app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get('/api/v1/tours',(req,res)=>{
-    res.status(200).json({
-        status:'success',
-        results:tours.length,
-        data:{tours}
-})
-})
 
-app.post('/api/v1/tours',(req,res)=>{
+const getAllTours = (req,res) =>{
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data:{tours}
+        
+    })
+}
+
+const getSingleTour = (req, res) => {
+    const id = req.params.id * 1;
+    const singleTour = tours.find(item=>
+        item.id === id)
+        if(!singleTour){
+            return res.status(404).json({
+                status:"Fail",
+                message:"Id Does not Exist"
+            })
+        }
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data:{tour:singleTour}
+    })
+}
+
+const createTour = (req, res) => {
     console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({newId},req.body);
+    const newTour = Object.assign({
+        newId
+    }, req.body);
     tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
         res
-        .status(201)
+            .status(201)
+            .json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                },
+            })
+    });
+}
+
+const updateTour = (req, res) => {
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status:"Fail",
+            message:"Id Does not Exist"
+        })
+    }
+        res
+        .status(200)
         .json({
             status: 'success',
-            data: {tour:newTour},
+            data: {
+                tour: `Patch stuff`
+            },
         })
-    });
-})
+};
+
+const deleteTour = (req, res) => {
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status:"Fail",
+            message:"Id Does not Exist"
+        })
+    }
+        res
+        .status(204)
+        .json({
+            status: 'success',
+            data: null
+        })
+};
+
+app.get('/api/v1/tours/', getAllTours);
+app.get('/api/v1/tours/:id', getSingleTour);
+app.post('/api/v1/tours',createTour );
+app.patch('/api/v1/tours/:id',updateTour );
+app.delete('/api/v1/tours/:id',deleteTour );
+
 
 const port = 3000;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`App running on PORT ${port}...`)
 })
